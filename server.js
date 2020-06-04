f29b8c121371a87f5168e0d0eb164196dfaec45b
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-06-05 00:31:12 +0300

    connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
+
+const connDB = require('./config/db');
 // Load env vars
 dotenv.config({
   path: './config/config.env',
 });
+
+// conenct DB
+connDB();
+
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 const middleware = (req, res, next) => {
@@ -17,4 +23,11 @@ if (process.env.NODE_ENV === 'development') app.use(middleware);
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {});
+const server = app.listen(PORT, () => {});
+
+// hadle unhandled rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.error(err.message);
+  console.error(promise);
+  server.close(() => process.exit(1));
+});
