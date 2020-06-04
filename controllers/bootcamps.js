05f2be24f448e96b5ff4c6f3fbc0bed08db0b57e
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-06-04 22:26:06 +0300

    create controller methods

---
+++ controllers/bootcamps.js
@@ -0,0 +1,22 @@
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootcamps',
+  });
+};
+
+exports.createBootcamps = (req, res, next) => {
+  console.log(req.params.id);
+  res.status(201).json({
+    success: true,
+    msg: 'create bootcamp',
+  });
+};
+
+exports.modifyBootcamp = (req, res, next) => {
+  console.log(req.params.id);
+  res.status(202).json({
+    success: true,
+    msg: 'modify bootcamp',
+  });
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all bootcamps',
-  });
-});
-router.post('/:id', (req, res) => {
-  console.log(req.params.id);
-  res.status(201).json({
-    success: true,
-    msg: 'Show all bootcamps',
-  });
-});
+const {
+  getBootcamps,
+  createBootcamps,
+  modifyBootcamp,
+} = require('../controllers/bootcamps');
+
+router.route('/').get(getBootcamps).post(createBootcamps);
+router.put('/:id', modifyBootcamp);
 
 module.exports = router;
