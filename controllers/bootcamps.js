1689cb2acef26a565bb5e3c322e8c5978217649b
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-06-05 10:32:11 +0300

    Fetching Get bootcamp(s)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,50 @@
 const Bootcamps = require('../models/Bootcamps');
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all bootcamps',
-  });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamps.find();
+    if (!bootcamps) {
+      res.status(400).json({
+        success: false,
+        error: 'do not have data',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: bootcamps,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.getBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamps.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+        error: 'do not have data',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 exports.createBootcamps = async (req, res, next) => {
+  console.log(Bootcamps);
   try {
     const bootcamp = await Bootcamps.create(req.body);
     res.status(201).json({
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,10 +6,12 @@ const {
   getBootcamps,
   createBootcamps,
   modifyBootcamp,
+  getBootcampById,
 } = require('../controllers/bootcamps');
 
 const jsonParser = bodyParser.json();
 router.route('/').get(getBootcamps).post(jsonParser, createBootcamps);
 router.put('/:id', modifyBootcamp);
+router.route('/:id').get(getBootcampById);
 
 module.exports = router;
